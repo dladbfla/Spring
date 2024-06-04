cef05ff212de738ee1ba9c0abd94f4664df53cf5
dladbfla <161662853+dladbfla@users.noreply.github.com>
2024-06-04 09:26:56 +0900

    6.3

--- gameshop2/src/main/resources/static/js/cart.js
+++ gameshop2/src/main/resources/static/js/cart.js
@@ -32,12 +32,32 @@ function sessionCurrent(data){
                 })
             }
         }
+
     })
     .catch((error)=>{
         console.log("에러 발생: ", error);
         alert("로그인해주세요.");
     })
 }
+function dlf(data){
+    axios
+    .get("http://localhost:8080/user/current", {withCredentials: true})
+    .then((response)=>{
+        console.log("데이터: ", response);
+        if(response.status == 200){
+            const userId = response.data.userId;
+            let cartItems = JSON.parse(localStorage.getItem(userId));
+            if(cartItems){
+                cartItems = cartItems.filter(item => item.id !== data.id);
+                localStorage.setItem(userId, JSON.stringify(cartItems));
+                window.location.reload();
+            }
+        }
+    })
+    .catch((error)=>{
+        console.log("에러 발생: ", error);
+    })
+};
 
 function displayCart(games){
     const tbody = document.querySelector(".cart-body");
@@ -49,27 +69,39 @@ function displayCart(games){
         const title = document.createElement("td");
         const genre = document.createElement("td");
         const price = document.createElement("td");
+        const dl = document.createElement("td");
+        const dlBtn = document.createElement("td");
         const img = document.createElement("img");
         //클래스이름 생성
         imgtd.classList.add("imgtd");
         img.classList.add("image");
+        dlBtn.classList.add("dlBtn");
         //태그 속성 추가
         img.src = data.image;
         title.textContent = data.title;
         genre.textContent = data.genre;
         price.textContent = data.price + "원";
+        dlBtn.textContent = "삭제";
         //appendChild 부모 자식 위치 설정
         imgtd.appendChild(img);
         tr.appendChild(imgtd);
         tr.appendChild(title);
         tr.appendChild(genre);
         tr.appendChild(price);
+        tr.appendChild(dl);
+        dl.appendChild(dlBtn);
         tbody.appendChild(tr);
 
         totalPrice = totalPrice + data.price;
+        dlBtn.addEventListener("click",()=>{
+            dlf(data);
+        })
     })
     document.querySelector(".totalprice").textContent = "총" + totalPrice + "원";
+    
 }
+
+
 
 
 //페이지 로딩시에 즉시 세션여부 확인
